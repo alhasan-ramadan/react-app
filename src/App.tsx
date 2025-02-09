f3f934f30f1c7600b4e0545d2d816c5d245a206d
Hasan <152815143+hadespreyz@users.noreply.github.com>
2025-02-09 20:23:02 +0100

    feat: added aurora effect but with black navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import NavigationBar from "./components/NavigationBar";
 import "./App.css";
+import Aurora from "./components/aurora/Aurora";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Aurora colorStops={["#50ff29", "#288d14", "#32ffb0"]} speed={0.5} />
     </div>
   );
 }
