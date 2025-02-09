fc4bc81266d0b9a779cca63e70c813832a7d994f
Hasan <152815143+hadespreyz@users.noreply.github.com>
2025-02-09 14:43:51 +0100

    feat: added dismissable button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,19 @@
 import Button from "./components/Button";
+import DismissableAlert from "./components/DismissableAlert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Button color="info" onClick={() => console.log("Click")}>
-        Click me
+      {alertVisible && (
+        <DismissableAlert onClose={() => setAlertVisible(false)}>
+          my Alert
+        </DismissableAlert>
+      )}
+      <Button color="dark" onClick={() => setAlertVisible(!alertVisible)}>
+        Toggle Alert
       </Button>
     </div>
   );
